8a29748a94cbb2ce1f54220d7bb5ec2ea528cfa2
Davidngy <davidngy2803gmail.com>
2024-04-17 19:00:11 +0200

    tried out NewWorkout

--- src/components/NewWorkout.jsx
+++ src/components/NewWorkout.jsx
@@ -7,26 +7,31 @@ import Exercise from '../components/Exercise';
 function MainPage() {
   const [workouts, setWorkouts] = useState([]);
   const [showModal, setShowModal] = useState(false);
+  const [showButton, setShowButton] = useState(true); // This controls the visibility of the button
 
   const addWorkout = (name) => {
     setWorkouts(prevWorkouts => [...prevWorkouts, { name }]);
     setShowModal(false); // Hide form after adding a workout
+    setShowButton(false); // Keep the button hidden after adding a workout
   }
 
   return (
     <>
       <h1 className='text-5xl mb-8'>Workout</h1>
       <Week />
-      {showModal ? (
-          <NewWorkout addWorkout={addWorkout} onClose={() => setShowModal(false)} />
-      ) : (
-          <button 
-              onClick={() => setShowModal(true)} 
-              className='p-2 text-lg border-2 border-black rounded-lg'>
-              + New Workout
-          </button>
+      {showButton && (
+        <button 
+          onClick={() => setShowModal(true)}
+          className='p-2 text-lg border-2 border-black rounded-lg'>
+          + New Workout
+        </button>
       )}
-
+      {showModal && (
+        <NewWorkout 
+          addWorkout={addWorkout} 
+          onClose={() => setShowModal(false)}
+        />
+      )}
 
       <div id='createWorkout' className='border-2 border-black rounded-lg text-left p-4'>
         <h2 className="text-4xl text-center font-bold">
