a18dec3cb78bbe7c3a42f4572198b61878e5e71d
Davidngy <davidngy2803gmail.com>
2024-04-23 19:35:49 +0200

    fixed merge conflicts in new_workout.js

--- backend/routes/new_workout.js
+++ backend/routes/new_workout.js
@@ -1,34 +1,4 @@
 import express from 'express';
-<<<<<<< HEAD
-const router = express.Router();
-
-// Middleware to parse JSON bodies
-router.use(express.json());
-
-router.post('/newworkoutname', (req, res) => {
-    // Extracting the workout name from the request body
-    const { name } = req.body;
-
-    // Here you might want to add logic to save the name to your database
-    //model save to user dates
-    //workout: name, user, date= one to many
-    //save the day of week tot workout as well
-
-    
-
-
-    console.log('Received workout name:', name);
-
-    // Sending a response back to the client
-    if (name) {
-        res.status(200).json({ message: 'Workout name saved successfully!', name });
-    } else {
-        res.status(400).json({ message: 'Workout name is required!' });
-    }
-});
-
-export default router;
-=======
 import User from '../DB/models/user.models.js';
 import Workout from '../DB/models/workout.models.js';
 
@@ -76,4 +46,4 @@ router.get('/api/workouts/:userId', async (req, res) =>
 });
 
 export default router;
->>>>>>> backend-workout
+
